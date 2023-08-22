e5937037b51bcb70cbaf9e3241a32986e16ed015
Waheed Afolabi <waheed.freelancer16@gmail.com>
2023-08-22 11:20:07 +0100

    Initial Commit

---
+++ InactivityTrackefr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InactivityTrackefr
+//
+//  Created by waheedCodes on 22/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
